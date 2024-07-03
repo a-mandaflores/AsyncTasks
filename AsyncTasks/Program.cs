1d0a8a193fea64a18a847b45222b34fd9a04a843
jersonb <jersonlbrito@hotmail.com>
2024-07-03 08:45:11 -0300

    init

---
+++ AsyncTasks/Program.cs
@@ -0,0 +1,29 @@
+﻿await Await10Seconds();
+await Await5Seconds();
+await Await1Second();
+
+await Task.WhenAll(Await10Seconds(), Await5Seconds(), Await1Second());
+
+var positionExecuted = Task.WaitAny(Await10Seconds(), Await5Seconds(), Await1Second());
+Console.WriteLine(positionExecuted);
+
+async Task Await10Seconds()
+{
+    Console.WriteLine($"start: {nameof(Await10Seconds)}");
+    await Task.Delay(TimeSpan.FromSeconds(10));
+    Console.WriteLine($"finish: {nameof(Await10Seconds)}");
+}
+
+async Task Await5Seconds()
+{
+    Console.WriteLine($"start: {nameof(Await5Seconds)}");
+    await Task.Delay(TimeSpan.FromSeconds(5));
+    Console.WriteLine($"finish: {nameof(Await5Seconds)}");
+}
+
+async Task Await1Second()
+{
+    Console.WriteLine($"start: {nameof(Await1Second)}");
+    await Task.Delay(TimeSpan.FromSeconds(1));
+    Console.WriteLine($"finish: {nameof(Await1Second)}");
+}
